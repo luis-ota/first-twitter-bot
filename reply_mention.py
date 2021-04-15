e9ec6117c1df9951991694a522fe85dfb8ed2d7c
luisotaviosilva <>
2021-04-15 01:04:40 +0000

    comitando pelo repl.it

--- reply_mention.py
+++ reply_mention.py
@@ -3,10 +3,10 @@ import time
 import logging
 import dotenv
 import os
-from datetime import datetime
 
 
-now = datetime.now()
+
+
 dotenv.load_dotenv(dotenv.find_dotenv())
 
 key = os.getenv("key")
@@ -21,12 +21,21 @@ api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
-
+reply=None
 
 class MyStreamListener(tweepy.StreamListener):
     def on_status(self, status):
-        logger.info("Retrieving mentions")
-        try:
+        global reply
+        try:  
+            user_do_mlk = str(status.text).split(" ")[0]          
+            def gerar_reply(): 
+                user_do_mlk = str(status.text).split(" ")[0]
+                if user_do_mlk[0] == '@' and user_do_mlk != '@MusicaFoxBot':
+                    reply = f"slk, {user_do_mlk} é abusado em campo https://twitter.com/i/status/1379858855933452288/video/1"
+                else:
+                    reply = "slk, mlk é abusado em campo https://twitter.com/i/status/1379858855933452288/video/1"
+            
+            reply = "slk, mlk é abusado em campo https://twitter.com/i/status/1379858855933452288/video/1"
             api.update_status(
                 status=reply, in_reply_to_status_id=status.id,
                 auto_populate_reply_metadata=True
@@ -34,16 +43,16 @@ class MyStreamListener(tweepy.StreamListener):
             
             logger.info(f"Answering to {status.user.name}")
             logger.info("Message sended")
-            
             time.sleep(169)
+            logger.info("Retrieving mentions")
         except tweepy.TweepError as e:
             print(e.reason, '--')
         except StopIteration:
-            print('deu ruim')
+            print('Erro')
+        
     
 if __name__ == "__main__":
-    logger.info(f"Bot started at {now}")
+    logger.info("Bot started")
     myStreamListener = MyStreamListener()
-    reply = "slk, mlk é abusado em campo https://twitter.com/i/status/1379858855933452288/video/1"
     myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
     myStream.filter(track=['#musicafoxbot', '#mscfoxbot'])
